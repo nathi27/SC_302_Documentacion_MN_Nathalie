c6de56356f574e4476d042ca62139e45468609f5
nathi27 <nathaliecham04@gmail.com>
2024-09-27 20:38:27 -0600

    Update mi-archivo.js

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,2 +1,3 @@
 // Este es el archivo de JS
-alert("hola mundo");
+alert("hola mundo_editado"); //Muestra una ventana emergente como una alerta 
+console.log("cambios en el archivo"); //Imprime el mensaje
